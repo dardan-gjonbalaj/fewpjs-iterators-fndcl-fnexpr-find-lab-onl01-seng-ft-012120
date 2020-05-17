0a168df389ddda37ae2d9e2465cfc5a66ab7ae6d
xdundun <xdundun@gmail.com>
2020-05-17 02:58:52 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result = array.find(keys => keys.result === "W")
+  return result ? result.year : undefined
+}
